adcd38e8a622edf81c0593d357f9b26ea396c336
tosca ragnini <tosca.ragnini@gmail.com>
2015-10-08 21:19:20 -0500

    added new site from sandbox
    this is the first update from the new work with the site update

---
+++ Sandbox/App_Start/RouteConfig.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sandbox
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+            routes.IgnoreRoute("{folder}/{*pathInfo}", new { folder = "Content" });
+
+            
+
+            routes.MapRoute(
+               "Index",
+               "Index",
+               new { controller = "Home", action = "Index" }
+               );
+
+            routes.MapRoute(
+               "Scribbles",
+               "Scribbles",
+               new { controller = "Home", action = "Scribbles" }
+               );
+
+            routes.MapRoute(
+               "Kids-Rings",
+               "Kids-Rings",
+               new { controller = "Home", action = "KidsRings" }
+               );
+
+            routes.MapRoute(
+               "Piercing-Pieces",
+               "Piercing-Pieces",
+               new { controller = "Home", action = "PiercingPieces" }
+               );
+
+            routes.MapRoute(
+               "New-Work",
+               "New-Work",
+               new { controller = "Home", action = "NewWork" }
+               );
+
+            routes.MapRoute(
+               "Resin-Balls",
+               "Resin-Balls",
+               new { controller = "Home", action = "ResinBalls" }
+               );
+
+            routes.MapRoute(
+               "Bump",
+               "Bump",
+               new { controller = "Home", action = "Bump" }
+               );
+
+            routes.MapRoute(
+               "Brood",
+               "Brood",
+               new { controller = "Home", action = "Brood" }
+               );
+
+            routes.MapRoute(
+               "Bulge",
+               "Bulge",
+               new { controller = "Home", action = "Bulge" }
+               );
+
+            routes.MapRoute(
+               "Boiling-Over",
+               "Boiling-Over",
+               new { controller = "Home", action = "BoilingOver" }
+               );
+
+            routes.MapRoute(
+               "Roach-Study",
+               "Roach-Study",
+               new { controller = "Home", action = "RoachStudy" }
+               );
+
+            routes.MapRoute(
+               "Crack",
+               "Crack",
+               new { controller = "Home", action = "Crack" }
+               );
+
+            routes.MapRoute(
+               "Shifting",
+               "Shifting",
+               new { controller = "Home", action = "Shifting" }
+               );
+
+            routes.MapRoute(
+               "Occupancy",
+               "Occupancy",
+               new { controller = "Home", action = "Occupancy" }
+               );
+
+            routes.MapRoute(
+               "Combat",
+               "Combat",
+               new { controller = "Home", action = "Combat" }
+               );
+
+            routes.MapRoute(
+               "Phenomenon",
+               "Phenomenon",
+               new { controller = "Home", action = "Phenomenon" }
+               );
+
+            routes.MapRoute(
+               "Metal-Pods",
+               "Metal-Pods",
+               new { controller = "Home", action = "MetalPods" }
+               );
+
+            routes.MapRoute(
+               "The-Mother-Load-Project",
+               "The-Mother-Load-Project",
+               new { controller = "Home", action = "TheMotherLoadProject" }
+               );
+
+            routes.MapRoute(
+               "Wannabe",
+               "Wannabe",
+               new { controller = "Home", action = "Wannabe" }
+               );
+
+            routes.MapRoute(
+               "Petite-Show",
+               "Petite-Show",
+               new { controller = "Home", action = "PetiteShow" }
+               );
+
+            routes.MapRoute(
+               "The-Merger",
+               "The-Merger",
+               new { controller = "Home", action = "TheMerger" }
+               );
+
+            routes.MapRoute(
+               "Trestle-Designs",
+               "Trestle-Designs",
+               new { controller = "Home", action = "TrestleDesigns" }
+               );
+
+            routes.MapRoute(
+               "about",
+               "about",
+               new { controller = "Home", action = "Index/#about" }
+               );
+
+            routes.MapRoute(
+               "contact",
+               "contact",
+               new { controller = "Home", action = "Index/#contact" }
+               );
+
+            routes.MapRoute(
+               "Default",
+               "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Sandbox/Controllers/HomeController.cs
@@ -0,0 +1,209 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sandbox.Controllers
+{
+    public class HomeController : Controller
+    {
+
+       public ActionResult Index()
+        {
+            return View();
+       }
+    
+       public ActionResult Scribbles()
+        {
+           return View();
+        }
+        public ActionResult KidsRings()
+        {
+
+            return View();
+        }
+        
+        public ActionResult PiercingPieces()
+        {
+
+            return View();
+        }
+        public ActionResult NewWork()
+        {
+
+            return View();
+        }
+        public ActionResult ResinBalls()
+        {
+
+            return View();
+        }
+        public ActionResult RoachStudy()
+        {
+
+            return View();
+        }
+        public ActionResult Bump()
+        {
+
+            return View();
+        }
+        public ActionResult Brood()
+        {
+
+            return View();
+        }
+        public ActionResult Bulge()
+        {
+
+            return View();
+        }
+        public ActionResult BoilingOver()
+        {
+
+            return View();
+        }
+        public ActionResult Crack()
+        {
+
+            return View();
+        }
+        public ActionResult Shifting()
+        {
+
+            return View();
+        }
+        public ActionResult Occupancy()
+        {
+
+            return View();
+        }
+
+        public ActionResult Combat()
+        {
+
+            return View();
+        }
+
+        public ActionResult Phenomenon()
+        {
+
+            return View();
+        }
+
+        public ActionResult MetalPods()
+        {
+
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult TheMotherLoadProject()
+        {
+
+            return View();
+        }
+        public ActionResult Wannabe()
+        {
+
+            return View();
+        }
+        public ActionResult PetiteShow()
+        {
+
+            return View();
+        }
+        public ActionResult TheMerger()
+        {
+
+            return View();
+        }
+        public ActionResult TrestleDesigns()
+        {
+
+            return View();
+        }
+
+
+      
+ 
+       
+    [HttpGet]
+        public ActionResult Contact()
+        { 
+            ViewBag.Message = "Your contact page.";
+            
+            return View(new ContactInfo());
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactInfo contact)
+        {
+
+            var fullName = contact.FullName;
+            var contactEmail = contact.ContactEmail;
+            var contactPhone = contact.ContactPhone;
+            var contactMessage = contact.ContactMessage;
+          SendEmail(fullName, contactEmail, contactPhone, contactMessage);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ContactSubmited()
+        {
+
+            var result = new FilePathResult("~/Views/Home/Index.html", "text/html");
+            return result;
+
+        }
+
+
+        public void SendEmail(string fullName, string contactEmail, string contactPhone, string contactMessage)
+        {
+            string mailgunKey = System.Configuration.ConfigurationManager.AppSettings["MAILGUN_API_KEY"];
+            if (String.IsNullOrEmpty(mailgunKey))
+            {
+                throw new Exception("Mailgun api key missing");
+            }
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+                    new HttpBasicAuthenticator("api",
+                                               mailgunKey);
+
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain",
+                                 System.Configuration.ConfigurationManager.AppSettings["MAILGUN_DOMAIN"], 
+                                 ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Natalie Macellaio - inquiry <mailgun@mailgun.org>");
+            request.AddParameter("to", "tosca.ragnini@gmail.com, nataliemacellaio@gmail.com");
+            request.AddParameter("subject", "Natalie Macellaio - Contact Request");
+            request.AddParameter("text", "FullName : " + fullName);
+            request.AddParameter("text", "ContactEmail : " + contactEmail);
+            request.AddParameter("text", "ContactPhone : " + contactPhone);
+            request.AddParameter("text", "ContactMessage : " + contactMessage);
+            request.Method = Method.POST;
+            var result = client.Execute(request);
+            return;
+        }
+
+
+    }
+
+
+    public class ContactInfo
+    {
+        public string FullName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactMessage { get; set; }
+
+    }  
+    
+}

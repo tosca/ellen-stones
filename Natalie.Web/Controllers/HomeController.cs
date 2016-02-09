6794d7e29a08e7c86a1fda188d4145ca7574ded3
tosca ragnini <tosca.ragnini@gmail.com>
2016-02-08 20:21:44 -0600

    removed mailgun, added google form

--- Natalie.Web/Controllers/HomeController.cs
+++ Natalie.Web/Controllers/HomeController.cs
@@ -140,19 +140,9 @@ namespace Sandbox.Controllers
         { 
             ViewBag.Message = "Your contact page.";
             
-            return View(new ContactInfo());
+            return View();
         }
-        [HttpPost]
-        public ActionResult Contact(ContactInfo contact)
-        {
-
-            var fullName = contact.FullName;
-            var contactEmail = contact.ContactEmail;
-            var contactPhone = contact.ContactPhone;
-            var contactMessage = contact.ContactMessage;
-          SendEmail(fullName, contactEmail, contactPhone, contactMessage);
-            return RedirectToAction("Index");
-        }
+       
 
         public ActionResult ContactSubmited()
         {
@@ -162,48 +152,10 @@ namespace Sandbox.Controllers
 
         }
 
-
-        public void SendEmail(string fullName, string contactEmail, string contactPhone, string contactMessage)
-        {
-            string mailgunKey = System.Configuration.ConfigurationManager.AppSettings["MAILGUN_API_KEY"];
-            if (String.IsNullOrEmpty(mailgunKey))
-            {
-                throw new Exception("Mailgun api key missing");
-            }
-            RestClient client = new RestClient();
-            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
-            client.Authenticator =
-                    new HttpBasicAuthenticator("api",
-                                               mailgunKey);
-
-            RestRequest request = new RestRequest();
-            request.AddParameter("domain",
-                                 System.Configuration.ConfigurationManager.AppSettings["MAILGUN_DOMAIN"], 
-                                 ParameterType.UrlSegment);
-            request.Resource = "{domain}/messages";
-            request.AddParameter("from", "Natalie Macellaio - inquiry <mailgun@mailgun.org>");
-            request.AddParameter("to", "tosca.ragnini@gmail.com");
-            request.AddParameter("subject", "Natalie Macellaio - Contact Request");
-            request.AddParameter("text", "FullName : " + fullName);
-            request.AddParameter("text", "ContactEmail : " + contactEmail);
-            request.AddParameter("text", "ContactPhone : " + contactPhone);
-            request.AddParameter("text", "ContactMessage : " + contactMessage);
-            request.Method = Method.POST;
-            var result = client.Execute(request);
-            return;
-        }
-
+         
 
     }
 
-
-    public class ContactInfo
-    {
-        public string FullName { get; set; }
-        public string ContactEmail { get; set; }
-        public string ContactPhone { get; set; }
-        public string ContactMessage { get; set; }
-
-    }  
+     
     
 }
